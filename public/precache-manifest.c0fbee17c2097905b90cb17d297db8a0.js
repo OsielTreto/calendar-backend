8c4b00acb1c42f7285527508997411a644afd0d0
OsielTreto <leizo194developer@gmail.com>
2021-02-12 12:01:19 -0600

    Se agrego la version de produccion del frontend

---
+++ public/precache-manifest.c0fbee17c2097905b90cb17d297db8a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf5af81332d33bfe86c2d04a2d9781d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2be79f90bcc8d65d7506",
+    "url": "/static/css/2.59a0ab78.chunk.css"
+  },
+  {
+    "revision": "8bd43091c971b93b95b1",
+    "url": "/static/css/main.5cf50011.chunk.css"
+  },
+  {
+    "revision": "2be79f90bcc8d65d7506",
+    "url": "/static/js/2.bd64b146.chunk.js"
+  },
+  {
+    "revision": "fa4b4056bba234820139274405351863",
+    "url": "/static/js/2.bd64b146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bd43091c971b93b95b1",
+    "url": "/static/js/main.a3f08cc2.chunk.js"
+  },
+  {
+    "revision": "c2ab9c45931d577cf16e",
+    "url": "/static/js/runtime-main.27b01e2e.js"
+  }
+]);
